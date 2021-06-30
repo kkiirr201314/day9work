af0f170af75ec359c7448302b6e1de2ae249fa1d
“kkiirr201314” <qq255073@gmail.com>
2021-07-01 00:18:16 +0800

    練習11-5

--- Tavern.kt
+++ Tavern.kt
@@ -1,35 +1,78 @@
 import java.io.File
-const val  TAVERN_NAME = "Taerny's Folly"
-var playerGold = 10
-var playerSilver = 10
+
+const val TAVERN_NAME = "Taernyl's Folly"
+
 val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
 val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
 val uniquePatrons = mutableSetOf<String>()
 val menuList = File("data/tavern-menu-items.txt")
     .readText()
     .split("\r\n")
-
-//val patronGold = mapOf("Eli" to 10.5, "Mordoc" to 8.0, "Sophie" to 5.5 )
-val patronGold = mutableMapOf<String,Double>()
-
+val patronGold = mutableMapOf<String, Double>()
+val leftPatron = mutableSetOf<String>()
 fun main() {
-    (0..9).forEach{
+    (0..9).forEach {
         val first = patronList.shuffled().first()
-        val last = lastName.shuffled().first()
+        val last = lastName. shuffled().first()
         val name = "$first $last"
         uniquePatrons += name
-
     }
-    uniquePatrons.forEach{
+    uniquePatrons.forEach {
         patronGold[it] = 6.0
     }
+
     var orderCount = 0
-    while (orderCount <= 9){
-        placeOrder(uniquePatrons.shuffled().first(),
-            menuList.shuffled().first())
+    while (orderCount <= 9) {
+        placeOrder(uniquePatrons.shuffled().first(), menuList.shuffled().first())
+        println("---------------------------------------------")
         orderCount++
     }
+
+    displayPatronBalances()
+    println("---------------------------------------------")
+
+    /*println("請問Eli在店內嗎?")
+    if (patronList.contains("Eli")) {
+        println("酒館老闆說: Eli在後面打牌")
+    } else {
+        println("酒館老闆說: Eli不再這裡")
+    }
+
+    println("---------------------------------------------")
+    println("請問Sophie, Mordoc在店內嗎?")
+    if (patronList.containsAll(listOf("Sophie", "Mordoc"))) {
+        println("酒館老闆說: 有阿，他們坐在燉鍋旁")
+    } else {
+        println("酒館老闆說: 沒有，他們幾小時前就離開了")
+    }
+
+    println("---------------------------------------------")*/
+    println("被請出酒館的客人有:")
+    patronGold.forEach { name, balance ->
+        if (balance <= 0) {
+            println(name)
+            leftPatron += name
+        }
+    }
+    println("---------------------------------------------")
+    println("離開客人Set:$leftPatron")
+    leftPatron.forEach {
+        patronGold -= it
+    }
+    println("剩下客人Map:$patronGold")
+    println("---------------------------------------------")
+    println("酒館裡剩下的客人有:")
+    patronGold.forEach { name, balance ->
+        println("$name，餘額為:${"%.2f".format(balance)}")
+    }
 }
+
+private fun displayPatronBalances() {
+    patronGold.forEach { patron, balance ->
+        println("$patron" + "的餘額為: ${"%.2f".format(balance)}")
+    }
+}
+
 private  fun performPurchase(price: Double, patronName: String){
     val totalPurse = patronGold.getValue(patronName)
     patronGold[patronName] = totalPurse - price
@@ -66,11 +109,4 @@ private  fun placeOrder(patronName: String, menuData: String){
         "$patronName 說道: 感謝 $name ."
     }
     println("$phrase \n")
-    displayPatronBalances()
-}
-private  fun  displayPatronBalances(){
-    patronGold.forEach{
-        patron,balance ->
-        println("$patron,balance: ${"%.2f".format(balance)}")
-    }
 }
